d26f5fbc12373b2d4331957cbeb455bfbf1cdaa6
araj2416 <araj24161999@gmail.com>
2023-07-05 07:28:24 +0530

    Add project files.

---
+++ AppointmentManagerWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppointmentManagerWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
