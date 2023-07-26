ab0cb942ed42dc339431f58a6a71c9a4821a2b99
araj2416 <araj24161999@gmail.com>
2023-07-26 11:30:00 +0530

    updated changes

--- AppointmentManagerWebApp/Data/ApplicationDbContext.cs
+++ AppointmentManagerWebApp/Data/ApplicationDbContext.cs
@@ -10,7 +10,7 @@ namespace AppointmentManagerWebApp.Data
             
         }
         public DbSet<User> Users { get; set; }
-        public DbSet<Test> Tests { get; set; }
+
         public DbSet<Doctor> Doctors { get; set; }
 
         public DbSet<Appointment> Appointments { get; set; }
